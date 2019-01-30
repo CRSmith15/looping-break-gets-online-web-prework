ec72c5556cb2354c6092388ba8acb3030c17208e
Chance Smith <chancesmith97@hotmail.com>
2019-01-30 00:33:17 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
   loop do
     puts "What is the spell that enacts levitation?"
-    answer == gets.chomp
+    answer = gets.chomp
     puts "#{answer}"
     break if answer == "Wingardium Leviosa"
   end
