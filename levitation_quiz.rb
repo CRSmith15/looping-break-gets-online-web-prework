eb12945d676cc09ab660660aa4bbbf0b2aa89b5e
Chance Smith <chancesmith97@hotmail.com>
2019-01-30 00:24:09 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,8 +5,9 @@ def levitation_quiz
       answer = gets.chomp
       puts "#{answer}"
       break if answer == "Wingardium Leviosa"
-      puts "You passed the quiz!"
     end
+    puts "You passed the quiz!"
+    
   end
 	#your code here
 end
